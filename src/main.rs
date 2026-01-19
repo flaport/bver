e41715829d87f814597a85a125366dbb81904e1f
flaport <floris.laporte@gmail.com>
2026-01-19 11:58:26 +0100

    read config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod loader;
 mod schema;
 
 use finders::find_project_root;
+use loader::load_config;
 
 fn main() {
-    println!("{:?}", find_project_root());
+    println!("project_root: {:?}", find_project_root());
+    println!("config: {:?}", load_config());
 }
